06562707ba570ec4b2566ca81ba213d5fb8285e7
Jesse-Driskill <jesse@driskill.com>
2023-11-05 23:47:19 -0800

    Adds styling

--- main.js
+++ main.js
@@ -1,5 +1,186 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import { famousPeople, quotes } from "./quotes";
+
+// let currentColor = {
+//     red: 128,
+//     green: 128,
+//     blue: 128
+// }
+// let previousColor = {
+//     red: null,
+//     green: null,
+//     blue: null
+// }
+
+// let nextColor = {
+//     red: null,
+//     green: null,
+//     blue: null
+// }
+
+let changingColor = false;
+
+function compareRGB(rgb1, rgb2) {
+    if (rgb1.red === rgb2.red && rgb1.green === rgb2.green && rgb1.blue === rgb2.blue) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div id="app">
+            <QuoteBoxContainer/>
+        </div>)
+    }
+}
+
+const QuoteBoxContainer = () => {
+    return (<div id="quote-box-container">
+        <QuoteBox/>
+    </div>)
+}
+
+class QuoteBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: "",
+            author: ""
+        }
+        this.generateNewQuote = this.generateNewQuote.bind(this);
+    }
+
+    generateNewQuote() {
+        let n = Math.floor(Math.random() * 100);
+        let quote = quotes[n];
+        let author = famousPeople[n];
+        
+        this.setState({
+            quote: quote,
+            author: author
+        });
+
+        function rRGB() {
+            return Math.floor(Math.random() * 190); 
+        }
+
+        let r = rRGB();
+        let g = rRGB();
+        let b = rRGB();
+
+        let colorString = `rgb(${r}, ${g}, ${b})`;
+
+        document.body.style.backgroundColor = colorString;
+        document.getElementById("new-quote-button").style.backgroundColor = colorString;
+        document.body.style.color = colorString;
+
+
+
+        
+
+    }
+
+    componentDidMount() {
+        this.generateNewQuote();
+    }
+
+    render() {
+        return (<div id="quote-box">
+            <Quote quote={this.state.quote} />
+            <QuoteAuthor author={this.state.author} />
+            <QuoteBoxFooter generateNewQuote={this.generateNewQuote}/>
+        </div>)
+    }
+}
+
+class Quote extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    
+
+    render() {
+        return (<h2 id="text">"{this.props.quote}"</h2>)
+    }
+}
+
+class QuoteAuthor extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div id="author">{this.props.author}</div>)
+    }
+}
+
+class QuoteBoxFooter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div id="quote-box-footer">
+        <SocialsContainer/>
+        <GenQuoteButton id="gen-quote-button" generateNewQuote={this.props.generateNewQuote}/>
+        </div>)
+    }
+}
+
+class GenQuoteButton extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.generateNewQuote = this.generateNewQuote.bind(this);
+    }
+
+    render() {
+        return (<button id="new-quote-button" onClick={this.props.generateNewQuote}>New Quote</button>)
+
+    }
+}
+
+class SocialsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div id="socials-container">
+        <TwitterIcon/>
+        <TumblrIcon/>
+    </div>)
+    }
+}
+
+class TwitterIcon extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<a href="twitter.com/intent/tweet" id="tweet-quote">
+            <img height="50px" src="assets/twitter.png"></img>
+        </a>)
+    }
+}
+
+class TumblrIcon extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        // return (<div id="tumblr">Tumblr Icon</div>)
+    }
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Hello World!");
+    let main = document.getElementById("main");
+    ReactDOM.render(<App/>, main);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
-const path = require('path')
+const path = require('path');
+
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'main.js'),
     output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+    }, module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    }
+                }
+            }
+        ]
     }
 }
